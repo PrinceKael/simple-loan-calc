f15dbb679958f954d9f500b81e8c56bea0a62920
PrinceKael <44752304+PrinceKael@users.noreply.github.com>
2023-10-30 06:39:26 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function calculateRepayment() {
-  loanAmountVal = document.getElementById("loan-amount").value;
+  principal = document.getElementById("loan-amount").value;
   interestRate = document.getElementById("interest-rate").value;
   monthsRemaining = document.getElementById("months-remaining").value;
   
   //First, multiply interest rate by 0.01 to convert to decimal/percentage, then divide by the months remaining and multiply by loan amount to amortize it.
-  interest = loanAmountVal * ((interestRate*0.01) / monthsRemaining);
+  interest = principal * ((interestRate*0.01) / monthsRemaining);
   
   //Monthly Payment = Loan Amount / Months Remaining + Interest, to 2 decimal places. (***redundant?) 
-  monthlyPayment = (loanAmountVal / monthsRemaining + interest).toFixed(2);
+  monthlyPayment = (principal / monthsRemaining + interest).toFixed(2);
 
   document.getElementById(
     "payment"
